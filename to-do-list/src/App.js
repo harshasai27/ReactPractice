b8bb2c97d0fe97a80d75d70fd50307f0cfc0d847
harshasai27 <bhadrisetty.v.sai@accenture.com>
2026-03-03 04:08:40 +0000

    rtl test cases along with context api theme

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from './context/ThemeContext';
 import SearchBar from './components/SearchBar';
 import TaskInput from './components/TaskInput';
 import TaskList from './components/TaskList';
@@ -8,6 +10,7 @@ function App() {
   const [task, setTask] = useState("");
   const [search, setSearch] = useState("");
   const [tasks, setTasks] = useState([]);
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   const addTask = () => {
     const cleanText = task.trim();
@@ -27,7 +30,7 @@ function App() {
   }
 
   const deleteTask = (id) => {
-    setTasks((prev) => prev.filter((task) => task.id != id));
+    setTasks((prev) => prev.filter((task) => task.id !== id));
   };
 
   const editTask = (id, newText) => {
@@ -37,7 +40,10 @@ function App() {
   const filteredTasks = tasks.filter((task) => task.text.toLowerCase().includes(search.toLowerCase()));
 
   return (
-    <div className='app-container'>
+    <div className='app-container'style={{
+      background : theme === "dark" ? "#111" : "#f3f4f6",
+      color : theme === "dark" ? "white" : "black"
+    }}>
       <h1 className='title'>To Do List</h1>
       <SearchBar search={search} setSearch={setSearch} />
       <TaskInput task={task} setTask={setTask} addTask={addTask} />
@@ -50,6 +56,10 @@ function App() {
       {filteredTasks.length === 0 && (
         <p className="error-message">No Tasks Found !</p>
       )}
+
+      <button onClick={toggleTheme}>
+          Switch to { theme === "light" ? "Dark" : "Light"} Mode
+      </button>
     </div>
   );
 
